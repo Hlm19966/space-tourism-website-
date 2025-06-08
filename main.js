ad8b541f90cd293904b719e29285fb7b8aef57da
Heli135 <164352976+Hlm19966@users.noreply.github.com>
2025-06-08 11:38:14 +0330

    added js for accessibility

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const iconHamburger = document.querySelector(".icon-hamburger");
+const hamburgerBtn = iconHamburger.closest("button");
 const mobileMenu = document.getElementById("mobileMenu");
 const closeIcon = document.querySelector(".close-icon");
 
+
+
+/*mobile menu*/
+
 iconHamburger.addEventListener("click", ()=> {
     mobileMenu.classList.add('active');
+    hamburgerBtn.setAttribute("aria-expanded", "true");
 });
 
 closeIcon.addEventListener("click", ()=> {
     mobileMenu.classList.remove('active');
+    hamburgerBtn.setAttribute("aria-expanded", "false");
 });
