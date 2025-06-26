5f75a28149e71abbf9dd5926f30dbdfd7759ae66
Heli135 <164352976+Hlm19966@users.noreply.github.com>
2025-06-26 10:58:32 +0330

    added the js and csss and html

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const iconHamburger = document.querySelector(".icon-hamburger");
 const hamburgerBtn = iconHamburger.closest("button");
 const mobileMenu = document.getElementById("mobileMenu");
 const closeIcon = document.querySelector(".close-icon");
-
+const planetButton = document.querySelectorAll(".tab-list-btn");
+const planetName = document.getElementById("planetName");
 
 
 /*mobile menu*/
@@ -15,4 +16,14 @@ iconHamburger.addEventListener("click", ()=> {
 closeIcon.addEventListener("click", ()=> {
     mobileMenu.classList.remove('active');
     hamburgerBtn.setAttribute("aria-expanded", "false");
+});
+
+
+
+
+planetButton.forEach((planet, index) => {
+    console.log(planetButton);
+    planetButton[1].addEventListener("click", ()=> {
+        alert("hi");
+    });
 });
